3daff786de1d73e0d7bd61f3395a900d27c69a9c
«Mikhail» <kopfgolova@gmail.com>
2022-05-26 21:48:49 +0300

    add Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+
+char [] Converting(int number)
+{
+    string remaindOfDivis = string.Empty;
+    while(number >= 1)
+    {
+        int ost = number % 2;
+        remaindOfDivis += ost.ToString();
+        number = number / 2;
+
+    }
+    return remaindOfDivis.Reverse().ToArray();
+}
+void PrintArray(char[]arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i]);
+}
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+char [] str = Converting(num);
+PrintArray(str);
+
